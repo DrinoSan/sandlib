b26296a0d8b593608437cc3a7f06479ac3be9180
DrinoSan <s.becirbegovic@gmail.com>
2026-05-31 23:41:28 +0200

    Removed assert and now only return and do nothing

--- Sand_string.c
+++ Sand_string.c
@@ -60,7 +60,11 @@ void sand_string_append( Sand_string_t* string, const char* str )
 void sand_string_append_n( Sand_string_t* string, const char* str, size_t len )
 {
    // I dont want strings of size 0 or smaller
-   assert( len > 0 );
+   //assert( len > 0 );
+   if( len <= 0 )
+   {
+      return;
+   }
 
    if ( ( int32_t ) ( string->size + len ) >= string->capacity )
    {
